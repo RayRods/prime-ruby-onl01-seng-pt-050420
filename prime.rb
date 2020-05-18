4685954073a4e03ded6ccb86b125fa34d9cecea4
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-18 00:15:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 def prime?(num)
     i = 2
+    return false if num <= 1
     while i < num
-        return false if num <= 1 && num < 1
+        return false if num % i == 0 && num < 1
         i += 1
     end
     true
