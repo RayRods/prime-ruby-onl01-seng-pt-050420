d720033132129c91ced512c8a4b8585e08617886
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-18 00:09:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,15 @@
 def prime?(num)
-    n = 2
-  if n < num
-    return false if num % n == 0
-    n += 1
-   else 
-     return true
-  end
- true
+ if num == 1
+    return true 
+  elsif num < 1
+    return false
+  else
+  for y in 2..(x-1)
+    if (x % y) == 0
+      return false 
+    end
+  end 
+end
+
+  true
 end
